c59a5228049c434c14b3205cffde46af4dbcf52c
jairolessa <jairo.rafaelcarvalho@gmail.com>
2026-02-07 21:01:54 -0300

    Adicionando metodo de busca por cpf

--- gestaoferias/src/main/java/gov/gestaoferias/gestaoferias/repository/PublicServerRepository.java
+++ gestaoferias/src/main/java/gov/gestaoferias/gestaoferias/repository/PublicServerRepository.java
@@ -3,5 +3,8 @@ package gov.gestaoferias.gestaoferias.repository;
 import gov.gestaoferias.gestaoferias.model.PublicServer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PublicServerRepository extends JpaRepository<PublicServer, Long> {
+    Optional<PublicServer> findByCpf(String cpf);
 }
